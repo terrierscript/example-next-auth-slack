61e939421e45c7de5acfa320a42f2b3ed61570e6
terrierscript <13282103+terrierscript@users.noreply.github.com>
2020-09-23 21:11:02 +0900

    remove

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
-
 const options = {
   // Configure one or more authentication providers
   providers: [
@@ -8,17 +7,7 @@ const options = {
       clientId: process.env.SLACK_CLIENT_ID,
       clientSecret: process.env.SLACK_CLIENT_SECRET
     })
-  ],
-  callbacks: {
-    signIn: async (user, account, profile) => {
-      console.log(user, account, profile)
-      return Promise.resolve(true)
-    },
-    session: async (session, user, sessionToken) => {
-      console.log(session, user, sessionToken)
-      return Promise.resolve(session)
-    }
-  }
+  ]
 }
 
 export default (req, res) => NextAuth(req, res, options)
